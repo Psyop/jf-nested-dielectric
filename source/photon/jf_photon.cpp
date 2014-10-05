982dc8b47cb7b5b8622a7f3fa75b79912f8b61c5
Jonah Friedman <jonahf@gmail.com>
2014-10-05 11:56:37 -0400

    reading photons into a std::vector

--- source/photon/jf_photon.cpp
+++ source/photon/jf_photon.cpp
@@ -1,9 +1,4 @@
 #include <ai.h>
-// #include <cstring>
-// #include <math.h>
-// #include <stdlib.h>
-// #include <iostream>
-// #include <fstream>
 #include <vector>
 
 #include <algorithm>
@@ -11,8 +6,7 @@
 #include <iterator>
 #include <vector>
 
-#include <windows.h>
-
+#include <ctime>
 
 
 AI_SHADER_NODE_EXPORT_METHODS(jf_photon_methods);
@@ -33,7 +27,6 @@ enum brdfs
 	m_write,
 };
 
-
 typedef struct photon_type{
 	AtColor energy;
 	AtVector positon;
@@ -45,11 +38,8 @@ typedef struct photon_type{
 typedef std::vector<photon_type> photon_cloud_type;
 
 struct ShaderData{
-	AtArray * thread_clouds;
-	photon_cloud_type * photon_cloud;
-	volatile int count;
-	volatile bool locked;
-	volatile bool initialized;
+	AtArray * write_thread_clouds;
+	photon_cloud_type * read_cloud;
 };
 
 
@@ -66,42 +56,88 @@ node_parameters {
 	AiParameterFlt("read_radius", 0.1f);
 }
 
+size_t init_cloud_size = sizeof(photon_type) * 100000;
  
 node_initialize {
 	ShaderData* data = new ShaderData;
 	AiNodeSetLocalData(node,data);
+
+	AtNode * renderOptions = AiUniverseGetOptions();
+
 	int mode = AiNodeGetInt(node, "mode");
+	
 	if (mode == m_write) {
-		AtNode * renderOptions = AiUniverseGetOptions();
-
-		AiMsgWarning("Setting up %d photon containers for the threads.", AiNodeGetInt(renderOptions, "threads"));
-
-		data->thread_clouds = AiArrayAllocate(AiNodeGetInt(renderOptions, "threads"), 1, AI_TYPE_POINTER);
-		for (AtUInt32 i = 0; i < data->thread_clouds->nelements; i++) {
+		// AiMsgWarning("Setting up %d photon containers for the threads.", AiNodeGetInt(renderOptions, "threads"));
+		data->write_thread_clouds = AiArrayAllocate(AiNodeGetInt(renderOptions, "threads"), 1, AI_TYPE_POINTER);
+		for (AtUInt32 i = 0; i < data->write_thread_clouds->nelements; i++) {
 			photon_cloud_type * cloud = new photon_cloud_type;
-			cloud->reserve(sizeof(photon_type) * 100000);
-			AiArraySetPtr(data->thread_clouds, i, cloud );
+			cloud->reserve(init_cloud_size);
+			AiArraySetPtr(data->write_thread_clouds, i, cloud );
 		}
+		return;
+	} 
+
+	if (mode == m_read) {
+
+		const clock_t photon_process_time = clock();
+
+		std::string string_path = AiNodeGetStr(node, "file_path");
+		std::ifstream infile (string_path, std::ios::binary);
+
+		AiMsgWarning("Reading Photon Cloud from: %s ", string_path.c_str());
+		if (!infile.good()) {
+			AiMsgError("Unable to read file! Check for invalid paths or bad permissions or something.");
+			return;
+		}
+
+		infile.seekg (0, infile.end);
+		size_t length = infile.tellg();
+		infile.seekg (0, infile.beg);
+
+		size_t num_photons = length/sizeof(photon_type);
+
+		data->read_cloud = new photon_cloud_type;
+		data->read_cloud->reserve(length);
+
+		// allocate memory:
+		photon_type* photon_array = new photon_type[num_photons];
+
+		char * buffer = (char*)(photon_array);
+		infile.read(buffer, length);
+
+		AiMsgWarning("The file is %d long, length %d, photons %d", infile.gcount(), length, num_photons);
+		photon_type* last_photon = &photon_array[num_photons - 1];
+		AiMsgWarning("Color of last photon %f %f %f", last_photon->energy.r, last_photon->energy.g, last_photon->energy.b );
+
+
+		// Data gets the photon array, and the number of photons
+		data->read_cloud->assign(photon_array, photon_array + num_photons);
+		AiMsgWarning("Photon vector contains %d photons", data->read_cloud->size());
+		last_photon = &data->read_cloud->at(num_photons - 1);
+		AiMsgWarning("Color of last photon %f %f %f", last_photon->energy.r, last_photon->energy.g, last_photon->energy.b );
+		delete photon_array;
+
+		AiMsgWarning("Photons read and processed: %f seconds", (float( clock () - photon_process_time ) /  CLOCKS_PER_SEC));
 	}
 }
  
 node_update {
 	int mode = AiNodeGetInt(node, "mode");
+
 	if (mode == m_write) {		
 		ShaderData *data = (ShaderData*)AiNodeGetLocalData(node);
 
-		for (AtUInt32 i = 0; i < data->thread_clouds->nelements; i++) {
-			photon_cloud_type * cloud = static_cast<photon_cloud_type*>(AiArrayGetPtr(data->thread_clouds, i));
+		for (AtUInt32 i = 0; i < data->write_thread_clouds->nelements; i++) {
+			photon_cloud_type * cloud = static_cast<photon_cloud_type*>(AiArrayGetPtr(data->write_thread_clouds, i));
 			cloud->clear();
-			cloud->reserve(sizeof(photon_type) * 10000);
+			cloud->reserve(init_cloud_size);
 		}
-
-		data->locked = false;
-		data->count = 0;
-		data->initialized = false;
-
+		return;
 	}
 
+	if (mode == m_read) {
+		// STUB
+	}
 }
 
 node_finish {
@@ -114,35 +150,38 @@ node_finish {
 
 	if (mode == m_write) {
 		std::string string_path = AiNodeGetStr(node, "file_path");
-
-		AiMsgWarning("Writing Photon Cloud to: %s ", string_path.c_str());
-
 		std::ofstream outfile (string_path, std::ios::binary);
 
+		AiMsgWarning("Writing Photon Cloud to: %s ", string_path.c_str());
 		if (!outfile.good()) {
-			AiMsgError("Unable to write file! Check for invalid paths of bad permissions or something.");
+			AiMsgError("Unable to write file! Check for invalid paths or bad permissions or something.");
 			return;
 		}
 
-		size_t real_photon_count = 0;
-		for (AtUInt32 i = 0; i < data->thread_clouds->nelements; i++) {
-			photon_cloud_type * cloud = static_cast<photon_cloud_type*>(AiArrayGetPtr(data->thread_clouds, i));
-			size_t cloud_size = cloud->size();
-			if (cloud_size > 0) {				
+		size_t photon_count = 0;
+		for (AtUInt32 i = 0; i < data->write_thread_clouds->nelements; i++) {
+			photon_cloud_type * cloud = static_cast<photon_cloud_type*>(AiArrayGetPtr(data->write_thread_clouds, i));
+
+			if (cloud->size() > 0) {				
 				outfile.write((const char*)&cloud->at(0), sizeof(photon_type) * cloud->size());
-				real_photon_count += cloud_size;
+				photon_count += cloud->size();
 			}
+
 			cloud->clear();
 			delete cloud;			
 		}
 
-		float megabytes = (float) (real_photon_count * sizeof(photon_type)) / (1024.0f*1024.0f);
-		AiMsgWarning("Photon cloud: %f mb, %d photons.", megabytes, real_photon_count);
+		float cloud_mb = (float) (photon_count * sizeof(photon_type)) / (1024.0f*1024.0f);
+		AiMsgWarning("Photon cloud: %f mb, %d photons.", cloud_mb, photon_count);
 
 		outfile.close();
-		AiArrayDestroy(data->thread_clouds);
-
+		AiArrayDestroy(data->write_thread_clouds);
 	}
+
+	if (mode == m_read) {
+		// STUB
+	}
+
 
 	delete data;
 }
@@ -156,7 +195,7 @@ shader_evaluate {
 
 		if (is_photon) {
 			photon_type photon = {photon_energy, sg->P, sg->Rt};
-			photon_cloud_type * cloud = static_cast<photon_cloud_type*>(AiArrayGetPtr(data->thread_clouds, sg->tid));
+			photon_cloud_type * cloud = static_cast<photon_cloud_type*>(AiArrayGetPtr(data->write_thread_clouds, sg->tid));
 			cloud->push_back(photon);
 		}
 	}
