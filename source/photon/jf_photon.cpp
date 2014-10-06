bfe925ccc0fc71560d432022d9f5cec93f98cb13
Jonah Friedman <jonahf@gmail.com>
2014-10-05 20:29:54 -0400

    octree appears to work

--- source/photon/jf_photon.cpp
+++ source/photon/jf_photon.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <iterator>
 #include <vector>
+#include <map>
 
 #include <ctime>
 
@@ -29,17 +30,202 @@ enum brdfs
 
 typedef struct photon_type{
 	AtColor energy;
-	AtVector positon;
-	// AtVector direction;
+	AtVector pos;
 	AtUInt16 type;
 } photon_type;
 
+typedef std::vector<photon_type> photon_cloud_type; // vector of photons with all the information a photon has
 
-typedef std::vector<photon_type> photon_cloud_type;
+typedef std::vector<size_t> photon_list_type; // a list of photons in a photon cloud
+
+
+
+
+typedef class photon_accellerator_type{
+	AtVector bounds_n;
+	AtVector bounds_p;
+
+	// AtVector _pos;
+	float _len;
+
+	bool _has_sub_accells;
+	size_t _recursion_level;
+	size_t _max_recursion;
+	size_t _max_per_bucket;
+	size_t _octant;
+
+	photon_list_type * _photon_list;
+	photon_accellerator_type * _sub_accells[8];
+
+	photon_cloud_type * _target_photon_cloud;
+
+	void init() {
+		_photon_list = new photon_list_type;
+	}
+
+	void set_bounds(size_t octant, AtVector par_bounds_n, float par_len) {
+		/*    0 means negative, 1 means positive
+		 * 0: 000
+		 * 1: 001
+		 * 2: 010
+		 * 3: 011
+		 * 4: 100
+		 * 5: 101
+		 * 6: 110
+		 * 7: 111
+		*/
+		float half_len = par_len/2.0f;
+		AtVector offset_vector = {half_len, half_len, half_len};
+		bool neg_x = octant % 2 == 0; //0, 2, 4
+		bool neg_y = (octant/2) % 2 == 0; //0, 1, 4, 5
+		bool neg_z = octant < 4;
+
+		bounds_n = par_bounds_n;
+		if (!neg_x) {
+			bounds_n.x += half_len;
+		}
+		if (!neg_y) {
+			bounds_n.y += half_len;
+		}
+		if (!neg_z) {
+			bounds_n.z += half_len;
+		}
+		bounds_p = bounds_n + offset_vector;
+		_len = half_len;
+
+	}
+
+	bool within_bounds(AtVector photon_pos) {
+		if (photon_pos.x >= bounds_n.x && 
+			photon_pos.y >= bounds_n.y && 
+			photon_pos.z >= bounds_n.z &&
+			photon_pos.x < bounds_p.x &&
+			photon_pos.y < bounds_p.y &&
+			photon_pos.z < bounds_p.z
+			) {
+			return true;
+		}
+		return false;
+	}
+
+	void add_ID_to_bucket(size_t ID) {
+		_photon_list->push_back(ID);
+	}
+
+	void initialize_child(photon_accellerator_type * child, size_t octant) {
+		child->init();
+		child->set_bounds(octant, bounds_n, _len); //will set _pos, bounds_n and bounds_p
+
+		child->_octant = octant;
+
+		child->_has_sub_accells = false;
+		child->_recursion_level = _recursion_level + 1;
+		child->_max_recursion = _max_recursion;
+		child->_max_per_bucket = _max_per_bucket;
+		
+		child->_photon_list = new photon_list_type;
+		child->_target_photon_cloud = _target_photon_cloud;
+
+		// if (octant != 0 && octant != 1)
+		// 	AiMsgWarning("Found an octant in initialization");
+	}
+
+
+	void build_structure() {
+		if (_photon_list->size() < _max_per_bucket || _recursion_level >= _max_recursion) {
+			// Stop building. Eitehr we're divided enough, or we're at our max recursion depth. 
+			if (_photon_list->size() > 0) {
+				AiMsgWarning("Recursion limit. %d points, %d depth, octant %d", _photon_list->size(), _recursion_level, _octant);
+			}
+			return;
+		}
+		// make 8 children and initialize them
+		for (size_t i = 0; i < 8; i++) {
+			_sub_accells[i] = new photon_accellerator_type;
+			initialize_child(_sub_accells[i], i);
+		}
+
+		// distribute points
+		for(size_t i = 0; i != _photon_list->size(); i++) {
+			size_t photon_ID = _photon_list->at(i);
+			for (size_t i = 0; i < 8; i++) {
+				AtVector photon_position = _target_photon_cloud->at(photon_ID).pos;
+				if (_sub_accells[i]->within_bounds(photon_position)) {
+					_sub_accells[i]->add_ID_to_bucket(photon_ID);
+
+					break;
+				}
+			}
+		}
+		_has_sub_accells = true;
+		delete _photon_list;
+
+		for (size_t i = 0; i < 8; i++) {
+			_sub_accells[i]->build_structure();
+		}
+	}
+	
+public:
+	void build(photon_cloud_type* photon_cloud, size_t max_per_bucket = 100, size_t max_nesting = 16) {
+		_target_photon_cloud = photon_cloud;
+		_max_per_bucket = max_per_bucket;
+		_max_recursion = max_nesting;
+		_recursion_level = 0;
+		_octant = -1;
+		init();
+
+		AtVector measured_bounds_n = AI_V3_ZERO;
+		AtVector measured_bounds_p = AI_V3_ZERO;
+
+		for(size_t i = 0; i != _target_photon_cloud->size(); i++) {
+			_photon_list->push_back(i);
+			photon_type * photon = &_target_photon_cloud->at(i);
+			if (i == 0) {
+				measured_bounds_n = photon->pos;
+				measured_bounds_p = photon->pos;
+			} else {
+				if (measured_bounds_n.x > photon->pos.x)
+					measured_bounds_n.x = photon->pos.x;
+				if (measured_bounds_n.y > photon->pos.y)
+					measured_bounds_n.y = photon->pos.y;
+				if (measured_bounds_n.z > photon->pos.z)
+					measured_bounds_n.z = photon->pos.z;
+
+				if (measured_bounds_p.x < photon->pos.x)
+					measured_bounds_p.x = photon->pos.x;
+				if (measured_bounds_p.y < photon->pos.y)
+					measured_bounds_p.y = photon->pos.y;
+				if (measured_bounds_p.z < photon->pos.z)
+					measured_bounds_p.z = photon->pos.z;
+			}
+		}
+		AtVector dim = measured_bounds_p - measured_bounds_n;
+
+		// _pos = measured_bounds_n;
+		_len = std::max(std::max(dim.x, dim.y), dim.z);
+		bounds_n = measured_bounds_n;
+		bounds_p = measured_bounds_p;
+
+		build_structure();
+	}
+	void destroy_structure() {
+		for (size_t i = 0; i < 8; i++) {
+			if (_sub_accells[i] != NULL) {
+				_sub_accells[i]->destroy_structure();
+			}
+		}
+	}
+} photon_accellerator_type;
+
+
+
+
+
 
 struct ShaderData{
 	AtArray * write_thread_clouds;
 	photon_cloud_type * read_cloud;
+	photon_accellerator_type * read_cloud_accelerator;
 };
 
 
@@ -79,7 +265,6 @@ node_initialize {
 
 	if (mode == m_read) {
 
-		const clock_t photon_process_time = clock();
 
 		std::string string_path = AiNodeGetStr(node, "file_path");
 		std::ifstream infile (string_path, std::ios::binary);
@@ -110,14 +295,32 @@ node_initialize {
 		AiMsgWarning("Color of last photon %f %f %f", last_photon->energy.r, last_photon->energy.g, last_photon->energy.b );
 
 
-		// Data gets the photon array, and the number of photons
-		data->read_cloud->assign(photon_array, photon_array + num_photons);
-		AiMsgWarning("Photon vector contains %d photons", data->read_cloud->size());
-		last_photon = &data->read_cloud->at(num_photons - 1);
+		const clock_t photon_process_time = clock();
+
+		photon_cloud_type * v_cloud = data->read_cloud; //Alias of the cloud in data->read_cloud;
+		
+		v_cloud->assign(photon_array, photon_array + num_photons);
+		AiMsgWarning("Photon vector contains %d photons", v_cloud->size());
+		last_photon = &v_cloud->at(num_photons - 1);
 		AiMsgWarning("Color of last photon %f %f %f", last_photon->energy.r, last_photon->energy.g, last_photon->energy.b );
 		delete photon_array;
 
+
+		// for(std::vector<photon_type>::iterator it = v_cloud->begin(); it != v_cloud->end(); ++it) {
+		// 	// AtVector myVec = it->pos;
+		//     if (abs(it->pos.x) < 0.0001) {
+		//     	AiMsgWarning("Photon with attitude!");
+		//     }
+		// }
+
+		photon_accellerator_type * accel = new photon_accellerator_type;
+		accel->build(v_cloud);
+
 		AiMsgWarning("Photons read and processed: %f seconds", (float( clock () - photon_process_time ) /  CLOCKS_PER_SEC));
+
+		
+		//AiMsgWarning("Lower bounds: %f, %f, %f", bounds_n.x, bounds_n.y, bounds_n.z);		
+		//AiMsgWarning("Upper bounds: %f, %f, %f", bounds_p.x, bounds_p.y, bounds_p.z);
 	}
 }
  
