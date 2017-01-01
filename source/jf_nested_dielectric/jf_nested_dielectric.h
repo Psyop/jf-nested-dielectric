8e5216c45b1bfcff0df9744b4a5bdb7f43b12bad
Jonah Friedman <jonahf@gmail.com>
2017-01-01 14:27:39 -0500

    changed names of messages

--- source/jf_nested_dielectric/jf_nested_dielectric.h
+++ source/jf_nested_dielectric/jf_nested_dielectric.h
@@ -13,12 +13,14 @@ typedef struct MediaFloatStruct { float v[max_media_count]; } MediaFloatStruct;
 typedef struct MediaBoolStruct { bool v[max_media_count]; } MediaBoolStruct;
 typedef struct MediaAtColorStruct { AtColor v[max_media_count]; } MediaAtColorStruct;
 
-#define JFND_MSG_VALID_BOOL "msgs_are_valid"
-#define JFND_MSG_RAYSTATE_PTR "rayState_ptr"
-#define JFND_MSG_SHADOW_VALID_BOOL "shadowlist_is_valid"
-#define JFND_MSG_PREV_TRANSP_INT "prev_transp_index"
-#define JFND_MSG_PHOTON_RGB "photon_energy"
-#define JFND_MSG_OPQ_SHADOW_MODE_BOOL "opaqueShadowMode"
+#define JFND_MSG_VALID_BOOL "JFND_MAV"
+#define JFND_MSG_RAYSTATE_PTR "JFND_RSP"
+#define JFND_MSG_SHADOW_VALID_BOOL "JFND_SHV"
+#define JFND_MSG_PREV_TRANSP_INT "JFND_PTI"
+#define JFND_MSG_OPQ_SHADOW_MODE_BOOL "JFND_OSM"
+
+// also used in jf_photon
+#define JFND_MSG_PHOTON_RGB "photon_energy" 
 
 #define JFND_MAX_TIR_DEPTH 50
 #define JFND_MAX_INVALID_DEPTH 70
